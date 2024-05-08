3a027508fe5eb45dfb57ed341a0a221d36bb6f26
fakbo <fakbo0206@gmail.com>
2024-05-07 20:28:33 -0400

    starting again

---
+++ src/Footer.jsx
@@ -0,0 +1,17 @@
+
+
+function Footer() {
+
+    return (
+        <>
+            <footer>
+                <p>
+                    &copy; {new Date().getFullYear()} Your Website Name
+                </p>
+            </footer>
+        </>
+    );
+}
+
+
+export default Footer;
