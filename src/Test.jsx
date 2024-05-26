8ded0b8dff1254f986a8f73808caf44f2f55cb1a
fakbo <fakbo0206@gmail.com>
2024-05-26 18:58:13 -0400

    ye

--- src/Test.jsx
+++ src/Test.jsx
@@ -1,40 +1,24 @@
-import { useRef } from "react"
-import { useState } from "react"
+import { useRef } from "react";
+
 function Test() {
 
-    const [startTime, setStartTime] = useState(null);
-    const [now, setNow] = useState(null);
-    const intervalRef = useRef(null);
+    const inputRef = useRef(null);
 
-    function handleStart() {
-
-        setStartTime(Date.now());
-        setNow(Date.now());
-
-        clearInterval(intervalRef.current);
-        intervalRef.current = setInterval(() => {
-            setNow(Date.now());
-        }, 10);
-
-    }
-
-    function handleStop() {
-        clearInterval(intervalRef.current);
-    }
-    let secondsPassed = 0;
-
-    if (startTime !== null && now !== null) {
-        secondsPassed = (now - startTime) / 1000;
-
+    function handleFocus() {
+        inputRef.current.focus();
     }
     return (
         <>
-            <h1>Tiempo transcurrido: {secondsPassed.toFixed(3)}</h1>
-            <button onClick={handleStart}>Iniciar</button>
-            <button onClick={handleStop}>Detener</button>
-            <button>yeah</button>
+            <nav>
+                <button onClick={handleFocus}>Buscar</button>
+            </nav>
+            <br />
+            <input
+                type="text"
+                placeholder="Buscando algo?"
+                ref={inputRef} />
         </>
-    );
+    )
 }
 
-export default Test;
+export default Test
